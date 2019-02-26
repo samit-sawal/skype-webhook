3f73b700cde2fdc7418d8b8b37f43c2eadc37795
Samit Sawal <Samit_Sawal@ad.infosys.com>
2019-02-26 21:51:48 +0530

    initial version

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()),// creates express http server
+  util = require('util'),
+  request = require('request');
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+  console.log('post req.body:' +JSON.stringify(req.body));
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('return from get webhook');
+
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+  
+  
+      res.status(200).send('return from get webhook');
+    
+    
+  
+});
