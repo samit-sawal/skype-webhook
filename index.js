32f1d71940b49fe564d4b4aac27ea47732272e72
samit-sawal <37258682+samit-sawal@users.noreply.github.com>
2019-03-05 11:22:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 app.post('/webhook', (req, res) => {  
  
   let body = req.body;
+  console.log('post req.headers:' +JSON.stringify(req.headers));
   console.log('post req.body:' +JSON.stringify(req.body));
-
+  
     // Returns a '200 OK' response to all requests
     res.status(200).send('return from get webhook');
 
